9f50e41aeb54e645afead3edcc7387597e71ff99
Marianne Girard <mariannegirard8@gmail.com>
2020-09-14 09:32:42 +0200

    fichier bdd done

--- data/blog.sql
+++ data/blog.sql
@@ -19,15 +19,14 @@ CREATE TABLE "user" (
 );
 
 CREATE TABLE "article" (
-  -- on utilise le nouveau type qui est un standard SQL alors que SERIAL est un pseudo-type de PG
+ 
   "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "title" TEXT NOT NULL DEFAULT '',
   "excerpt" TEXT NOT NULL DEFAULT'',
   "content"TEXT NOT NULL DEFAULT'',
   "image_url" TEXT VALID URL-DEFAULT'/default-img.jpg',
   "state" TEXT NOT NULL DEFAULT 'approved',
-  -- pour avoir la date et l'heure on utilise le type "timestamp", et pour être le plus précis possible on utilisera plutôt le type "timestampz" qui contient en plus de la date et de l'heure le fuseau horaire défini dans les locales du serveur
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   "updated_at" TIMESTAMPTZ,
   "category_id" INTEGER NOT NULL,
    "user_id" INTEGER NOT NULL,
@@ -46,20 +45,18 @@ CREATE TABLE "application" (
   "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "content"TEXT NOT NULL DEFAULT'',
   "state" TEXT NOT NULL DEFAULT 'approved',
-  -- pour avoir la date et l'heure on utilise le type "timestamp", et pour être le plus précis possible on utilisera plutôt le type "timestampz" qui contient en plus de la date et de l'heure le fuseau horaire défini dans les locales du serveur
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   "updated_at" TIMESTAMPTZ,
    "user_id" INTEGER NOT NULL,
 
 
 /* une fois les tables crées, on va les remplir */
-/* une fois les tables crées, on va les remplir */
 INSERT INTO "article" ("title","excerpt","content","image_url")
-VALUES ('article' );
+VALUES ('title','excerpt','content','image');
 INSERT INTO "category" ("name", "image_url", "list_id")
-VALUES ('category'),   
+VALUES ('name','image',1),   
 INSERT INTO "user" ("firstname","lastname","email","password","role","avatar_url")
-VALUES ('user';
+VALUES ('firstname','lastname','email','password','role','avatar');
 INSERT INTO "application"("content","state")
 VALUES('content','state');
 
